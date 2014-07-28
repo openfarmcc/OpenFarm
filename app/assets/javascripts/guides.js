6bb737654cbe9d52948f721a648d37a932b4e308
Simon <svansintjan@gmail.com>
2014-07-28 10:46:21 +0100

    Quick update to accordion filler.
    This is a work in progress, so ignore for now, but made it less copy-pasty.

--- app/assets/javascripts/guides.js
+++ app/assets/javascripts/guides.js
@@ -1,27 +1,7 @@
 $(function() {
     var availableTags = [
-      "ActionScript",
-      "AppleScript",
-      "Asp",
-      "BASIC",
-      "C",
-      "C++",
-      "Clojure",
-      "COBOL",
-      "ColdFusion",
-      "Erlang",
-      "Fortran",
-      "Groovy",
-      "Haskell",
-      "Java",
-      "JavaScript",
-      "Lisp",
-      "Perl",
-      "PHP",
-      "Python",
-      "Ruby",
-      "Scala",
-      "Scheme"
+      "Tomato",
+      "Corn",
     ];
     $( "#tags" ).autocomplete({
       source: availableTags

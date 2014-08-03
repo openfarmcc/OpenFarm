3a502e7bea3a14699adbd707cd57a6405c8e0dd3
Simon <svansintjan@gmail.com>
2014-08-03 17:42:12 +0100

    Work on the layout of guides

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,7 +17,15 @@
 //= require_tree .
 
 $(function(){ 
-	$(document).foundation(); 
+	$(document).foundation({
+		  accordion: {
+		    // specify the class used for active (or open) accordion panels
+		    active_class: 'active',
+		    // allow multiple accordion panels to be active at the same time
+		    multi_expand: true,
+		    // allow accordion panels to be closed by clicking on their headers
+		    // setting to false only closes accordion panels when another is opened
+		    toggleable: true
+		  }
+		});
 });
-
-$(function(){ $(document).foundation(); });
--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -8,6 +8,42 @@ class GuidesController < ApplicationController
   end
 
   def show
+    # TODO => season/dates are dependent on locality.
+    @seasons = {
+      'Winter' => 'Dec 22 - Mar 21',
+      'Spring' => 'Mar 22 - Jun 21',
+      'Summer' => 'Jun 22 - Sep 21',
+      'Fall'   => 'Sep 22 - Dec 21'
+    }
+    @months = ['Jan', 'Feb', 'Mar', 'Apr', 'May',
+              'Jun', 'Jul', 'Aug', 'Sep', 'Oct',
+              'Nov', 'Dec']
+    @stages = {
+      'Prep'        => {
+          "days" => '',
+          },
+      'Sow'         => {
+          "days" => 'Day 0',
+          },
+      'Germination' => {
+          "days" => 'Days 1-6' 
+          },
+      'Seedling'    => {
+          "days" => 'Days 7-20'
+          },
+      'Juvenile'    => {
+          "days" => 'Days 21-34',
+          },
+      'Adult'       => {
+          "days"  => 'Days 35-57'
+          },
+      'Flower'      => {
+          "days"  => 'Days 58-71'
+          },
+      'Fruit'       => {
+          "days"  => 'Days 72-85'
+        }
+    }
     @guide = Guide.find(params[:id])
   end
   

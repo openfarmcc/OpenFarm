e12b4468ad45295870d0c60c331abb3810d66478
Rick Carlino <rick.carlino@gmail.com>
2014-08-19 07:09:48 -0500

    Opps

--- spec/features/crop_searches_spec.rb
+++ spec/features/crop_searches_spec.rb
@@ -5,20 +5,15 @@ describe 'Crop search', :type => :controller do
 
   it 'finds documents' do
     visit root_path
-    fill_in 'cropsearch_searchtext', with: 'radish'
+    fill_in 'q', with: 'radish'
     click_button 'Search!'
     expect(page).to have_content('Horseradish (Armoracia rusticana, syn. Cochlearia armoracia)')
   end
 
   it 'handles empty searches' do
     visit root_path
-    fill_in 'cropsearch_searchtext', with: ''
+    fill_in 'q', with: ''
     click_button 'Search!'
-    current_url.should == root_url
+    expect(page).to have_content('Horseradish (Armoracia rusticana, syn. Cochlearia armoracia)')
   end
-
-  it 'handles pagination'
-
-  it 'paginates results'
-
 end

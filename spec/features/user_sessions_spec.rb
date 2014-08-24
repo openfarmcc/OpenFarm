6657ddcc1ce17cc5a5e51f20a66cbf21371bd2a8
Rick Carlino <rick.carlino@gmail.com>
2014-08-24 09:21:58 -0500

    One new spec

--- spec/features/user_sessions_spec.rb
+++ spec/features/user_sessions_spec.rb
@@ -3,11 +3,20 @@ require "spec_helper"
 describe "User sessions" do
   include IntegrationHelper
   let(:user) { FactoryGirl.create(:user) }
-
   it "logs in" do
-    pending("do this next")
     visit root_path
     click_link "Create an Account"
+    fill_in :user_display_name, with: 'Rick'
+    fill_in :user_password, with: 'passwerd'
+    fill_in :user_password_confirmation, with: 'passwerd'
+    fill_in :user_email, with: 't@g.com'
+    fill_in :user_email, with: 't@g.com'
+    fill_in :user_location, with: 'Portland'
+    fill_in :user_soil_type, with: 'rocky'
+    fill_in :user_years_experience, with: '3'
+    fill_in :user_preferred_growing_style, with: 'manure composting'
+    click_button "Create User"
+    expect(page).to have_content("Welcome! You have signed up successfully.")
   end
 
   it "logs out" do

d45faf8e2695d5c11de74e03876fabaa665e2a21
Rick Carlino <rick.carlino@gmail.com>
2014-08-27 06:42:22 -0500

    Unit tests for crops controller

--- app/controllers/api/crops_controller.rb
+++ app/controllers/api/crops_controller.rb
@@ -2,7 +2,7 @@ module Api
   class CropsController < Api::Controller
     skip_before_action :authenticate_user!, only: :index
     def index
-      if params[:query].present? && (params[:query].length > 3)
+      if params[:query].present? && (params[:query].length > 2)
         render json: Crop.full_text_search(params[:query]).limit(5)
       else
         render json: []

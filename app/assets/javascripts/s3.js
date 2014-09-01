c93310cc013233054af959996d549de722499b8f
Rick Carlino <rick.carlino@gmail.com>
2014-08-31 19:17:29 -0500

    WIP

--- app/assets/javascripts/s3.js
+++ app/assets/javascripts/s3.js
@@ -1,5 +1,6 @@
 var s3upload = angular.module('s3upload', ['ngS3upload']);
 
 s3upload.controller('s3ctrl', function s3ctrl($scope) {
-  $scope.q = 'q';
+  $scope.bucket= "farmbot";
+  $scope.q = {};
 });
--- app/controllers/api/guides_controller.rb
+++ app/controllers/api/guides_controller.rb
@@ -11,10 +11,11 @@ module Api
 
     def s3
       # AKIAJ6RMTJ4UZZLRJS7A
+      # GKy7MnL4EbLboLUqL/p2NTbRR5AyS8IrmhiS1ms+
       render json: {
         policy:    s3_upload_policy,
         signature: s3_upload_signature,
-        key:       'GKy7MnL4EbLboLUqL/p2NTbRR5AyS8IrmhiS1ms+'
+        key:       'AKIAJ6RMTJ4UZZLRJS7A'
       }
     end
 private
@@ -40,7 +41,7 @@ private
     end
 
     def s3_upload_signature
-      Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest::Digest.new('sha1'), 'GKy7MnL4EbLboLUqL/p2NTbRR5AyS8IrmhiS1ms+', s3_upload_policy)).gsub("\n","")
+      Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest::Digest.new('sha1'), 'AKIAJ6RMTJ4UZZLRJS7A', s3_upload_policy)).gsub("\n","")
     end
 # = = = = =
   end

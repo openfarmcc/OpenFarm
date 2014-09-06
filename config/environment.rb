c13c4d0279b39144be4558a8340f5dcb2e177a56
Rick Carlino <rick.carlino@gmail.com>
2014-09-06 10:51:46 -0500

    Opps.

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,10 @@
 if File.exists?('config/app_environment_variables.rb')
   load 'config/app_environment_variables.rb'
-else
-  warning = "You still need to set your ENV variables inside of"\
-            " config/app_environment_variables.rb Take a look at "\
-            "config/app_environment_variables.rb.example"
-  raise warning
+# else
+#   warning = "You still need to set your ENV variables inside of"\
+#             " config/app_environment_variables.rb Take a look at "\
+#             "config/app_environment_variables.rb.example"
+#   raise warning
 end
 
 # Load the Rails application.

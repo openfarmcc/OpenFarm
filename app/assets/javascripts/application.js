52670403c08f6b4f733c50362f18f6790a92754b
Rick Carlino <rick.carlino@gmail.com>
2014-09-08 06:14:50 -0500

    Merging

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,3 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
-//abide, clearing, interchange, joyride, magellan, offcanvas, orbit, reveal, slider, tab, and equalizer
 //= require jquery
 //= require jquery_ujs
 //- require jquery-ui
@@ -30,6 +18,8 @@
 //= require_tree ./guides
 //= require delete_me_later
 
+
+//TODO: Need a better folder structure
 $(function(){ 
 	$(document).foundation({
 		  accordion: {
--- app/assets/javascripts/delete_me_later.js
+++ app/assets/javascripts/delete_me_later.js
@@ -1,3 +1,4 @@
+// This is a testing ground that @rickCarlino and @simonv3 use for S3 testing
 var s3upload = angular.module('s3upload', ['ngS3upload']);
 
 var s3upload_controller = function ($scope) {

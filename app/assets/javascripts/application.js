465f7a654c0b6fad5c6a09e95d53cce1433479e2
Rick Carlino <rick.carlino@gmail.com>
2014-09-07 20:31:44 -0500

    Remove unused foundation plugins from application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,7 @@
 //
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+//abide, clearing, interchange, joyride, magellan, offcanvas, orbit, reveal, slider, tab, and equalizer
 //= require jquery
 //= require jquery_ujs
 //- require jquery-ui
@@ -20,22 +20,11 @@
 //= require foundation
 //= require foundation/custom.modernizr
 //= require foundation/foundation
-//= require foundation/foundation.abide
 //= require foundation/foundation.accordion
 //= require foundation/foundation.alert
-//= require foundation/foundation.clearing
 //= require foundation/foundation.dropdown
-//= require foundation/foundation.interchange
-//= require foundation/foundation.joyride
-//= require foundation/foundation.magellan
-//= require foundation/foundation.offcanvas
-//= require foundation/foundation.orbit
-//= require foundation/foundation.reveal
-//= require foundation/foundation.slider
-//= require foundation/foundation.tab
 //= require foundation/foundation.tooltip
 //= require foundation/foundation.topbar
-//= require foundation/foundation.equalizer
 //= require foundation/mm-foundation-0.3.0.js
 //= require foundation/mm-foundation-tpls-0.3.0.js
 //= require_tree ./guides

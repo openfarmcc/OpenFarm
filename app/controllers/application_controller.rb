dfbb73a5cdf6a4359a405f6531b2e31472a552e9
Rick Carlino <rick.carlino@gmail.com>
2014-09-09 08:05:41 -0500

    indent access mod

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   # Allow certain fields for devise - needed in Rails 4.0+
   before_filter :update_sanitized_params, if: :devise_controller?
 
-protected
+  protected
 
   # This method allows devise to pass non standard attributes through and
   # thereby comply with 'strong parameters'.

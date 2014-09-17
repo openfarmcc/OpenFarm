b8234e5058079831ff393883c994db36066e4821
Rick Carlino <rick.carlino@gmail.com>
2014-09-17 07:47:26 -0500

    Switch Guides controller to mutations

--- app/mutations/guides/update_guide.rb
+++ app/mutations/guides/update_guide.rb
@@ -1,5 +1,8 @@
 module Guides
   class UpdateGuide < Mutations::Command
+
+    include Guides::GuidesConcern
+
     required do
       string :id
       model :user
@@ -22,8 +25,6 @@ module Guides
       guide
     end
 
-    private
-
     def validate_permissions
       if guide.user != user
         # TODO: Make a custom 'unauthorized' exception that we can rescue_from
@@ -34,28 +35,12 @@ module Guides
       end
     end
 
-    def validate_image_url
-      if featured_image.present? && !valid_url?(featured_image)
-        add_error :featured_image,
-                  :invalid_url,
-                  'Must be a fully formed URL, including the HTTP:// or '\
-                  'HTTPS://'
-      end
-    end
-
     def set_valid_params
       # TODO: Probably a DRYer way of doing this.
       guide.overview       = overview if overview.present?
       guide.name           = name if name.present?
       guide.featured_image = featured_image if featured_image.present?
       guide.save
-    end
-
-    def valid_url?(url)
-      uri = URI.parse(url)
-      uri.is_a?(URI::HTTP)
-    rescue URI::InvalidURIError
-      false
     end
   end
 end

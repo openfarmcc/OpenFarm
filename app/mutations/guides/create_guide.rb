f590909f3d984f5f9723b904d62e5ccf603842f0
Rick Carlino <rick.carlino@gmail.com>
2014-09-17 23:04:37 -0500

    Add seperate mutations

---
+++ app/mutations/guides/create_guide.rb
@@ -0,0 +1,50 @@
+module Guides
+  class CreateGuide < Mutations::Command
+
+    attr_writer :guide
+
+    include Guides::GuidesConcern
+
+    required do
+      model :user
+      string :crop_id
+      string :name
+    end
+
+    optional do
+      string :overview
+      string :featured_image
+    end
+
+    def guide
+      @guide ||= Guide.new
+    end
+
+    def validate
+      validate_crop
+      validate_image_url
+    end
+
+    def execute
+      set_params
+      guide
+    end
+
+    def set_params
+      guide.crop           = @crop
+      guide.name           = name
+      guide.user           = user
+      guide.overview       = overview if overview
+      guide.featured_image = featured_image if featured_image
+    end
+
+    def validate_crop
+      @crop = Crop.find(crop_id)
+    rescue Mongoid::Errors::DocumentNotFound
+      add_error :crop_id,
+                :crop_not_found,
+                "Could not find a crop with id #{crop_id}."
+    end
+
+  end
+end

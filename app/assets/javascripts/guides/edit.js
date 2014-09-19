edec69d974e0d87eda29464700ba8983789b128c
Simon <svansintjan@gmail.com>
2014-09-19 18:53:21 +0700

    doing work for #92

--- app/assets/javascripts/guides/edit.js
+++ app/assets/javascripts/guides/edit.js
@@ -34,3 +34,26 @@ editGuidesApp.controller('editGuideCtrl', ['$scope', '$http',
       })
     }
 }]);
+
+editGuidesApp.directive('focusMe', function($timeout, $parse) {
+  return {
+    //scope: true,   // optionally create a child scope
+    link: function(scope, element, attrs) {
+      var model = $parse(attrs.focusMe);
+      scope.$watch(model, function(value) {
+        console.log('value=',value);
+        if(value === true) { 
+          $timeout(function() {
+            element[0].focus(); 
+          });
+        }
+      });
+      // to address @blesh's comment, set attribute value to 'false'
+      // on blur event:
+      element.bind('blur', function() {
+         console.log('blur');
+         scope.$apply(model.assign(scope, false));
+      });
+    }
+  };
+});

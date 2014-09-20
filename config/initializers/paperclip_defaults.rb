7315c3860da5638f5c781c792e719c9e730f2884
Rick Carlino <rick.carlino@gmail.com>
2014-09-20 18:02:55 -0500

    Works. Need to fix tests.

--- config/initializers/paperclip_defaults.rb
+++ config/initializers/paperclip_defaults.rb
@@ -1,6 +1,6 @@
 options = { storage: :s3,
             path: '/:rails_env/media/:class/:attachment/:id.:extension',
-            s3_credentials: { bucket: ENV['MASTER_BUCKET_NAME'],
-                              access_key_id: ENV['MASTER_ACCESS_KEY'],
-                              secret_access_key: ENV['MASTER_SECRET_KEY']} }
+            s3_credentials: { bucket: ENV['S3_BUCKET_NAME'],
+                              access_key_id: ENV['SERVER_S3_ACCESS_KEY'],
+                              secret_access_key: ENV['SERVER_S3_SECRET_KEY']} }
 Paperclip::Attachment.default_options.merge!(options)

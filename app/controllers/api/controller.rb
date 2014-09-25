f776300e91dd202f0e3ddf24c72fa37b9c39856d
Rick Carlino <rick.carlino@gmail.com>
2014-09-24 20:11:29 -0500

    Locked down API controller with Tokens

--- app/controllers/api/controller.rb
+++ app/controllers/api/controller.rb
@@ -1,9 +1,24 @@
 class Api::Controller < ActionController::Base
   protect_from_forgery with: :null_session
+  skip_before_action :verify_authenticity_token
+
   respond_to :json
-  before_action :authenticate_user!
+
+  before_action :authenticate_from_token!
 
   protected
+
+  def authenticate_from_token!
+    authenticate_or_request_with_http_token do |token, options|
+      user = Token.get_user(token)
+      if user
+        sign_in user, store: false
+      else
+        # TODO Use rescue_from hooks for all of these errors.
+        render json: {error: 'unauthorized.'}, status: :unauthorized
+      end
+    end
+  end
 
   def respond_with_mutation(status = :ok)
     if @outcome.success?
--- app/models/token.rb
+++ app/models/token.rb
@@ -1,12 +1,33 @@
 class Token
   include Mongoid::Document
-  before_create :generate_access_token
 
-  field :encrypted_token, type: String
+  after_initialize :generate_access_token
 
+  attr_reader :plaintext
+
+  field :secret
+  field :expiration, type: Time, default: ->{ Time.current + 1.month }
+  validates :secret, :expiration, :user_id, presence: true
+  belongs_to :user
   private
 
   def generate_access_token
-    self.access_token = Digest::SHA256.hexdigest(Time.now.to_s)
+      # Generate the secret
+      @plaintext  = SecureRandom.hex
+      # Encrypt the secret (in case the DB gets hacked.)
+      self.secret = Digest::SHA256.hexdigest(SecureRandom.hex)
+  end
+
+  # (String) plaintext: An email address, then a colon (':') then a token in
+  # plaintext. Example: "rick@me.com:1234567"
+  # Returns User object on success. Otherwise `false`
+  def self.get_user(token)
+    email, plaintext = *token.split(':')
+    user = email && User.find_by(email: email)
+    guess = Digest::SHA256.hexdigest(plaintext)
+    if user && Devise.secure_compare(user.token.secret, guess)
+      return user
+    end
+    false
   end
 end

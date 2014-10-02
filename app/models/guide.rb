85f9419850f6af9d6603a22cb701195c6fe75a80
Simon <svansintjan@gmail.com>
2014-10-02 08:38:00 +0700

    Updates to image uploading

--- app/models/guide.rb
+++ app/models/guide.rb
@@ -20,7 +20,7 @@ class Guide
   validates_presence_of :user, :crop, :name
 
   has_mongoid_attached_file :featured_image,
-                            default_url: 'https://openfarm.cc/img/page.png'
+                            default_url: 'leaf-grey.png'
   validates_attachment_size :featured_image, in: 1.byte..2.megabytes
   validates_attachment :featured_image,
                        content_type: { content_type:

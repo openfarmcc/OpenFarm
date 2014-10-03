5a327db70ccd6fb32eb9228ab5add6d0eec7b55f
Rick Carlino <rick.carlino@gmail.com>
2014-10-03 06:29:24 -0500

    Importer bug fix

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -1,4 +1,4 @@
-class AnnouncementsController < Api::Controller
+class AnnouncementsController < ApplicationController
   def hide
     session[:announcement_hide_time] = Time.now
     render nothing: true, status: :no_content
--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -1,4 +1,5 @@
 class GuidesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @guides = Guide.all

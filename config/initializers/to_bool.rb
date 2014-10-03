dc8835c177d6c49271774488c38383b0626cc8ca
Rick Carlino <rick.carlino@gmail.com>
2014-10-03 07:13:20 -0500

    More hound

--- config/initializers/to_bool.rb
+++ config/initializers/to_bool.rb
@@ -24,10 +24,17 @@ class TrueClass
 end
 
 class FalseClass
-  def to_i; 0; end
-  def to_bool; self; end
+  def to_i
+    0
+  end
+
+  def to_bool
+    self
+  end
 end
 
 class NilClass
-  def to_bool; false; end
+  def to_bool
+    false
+  end
 end

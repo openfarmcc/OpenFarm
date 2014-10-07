3831aec3f96c1c72f038141211e358747eeb08fd
digital dreamer <digitaldreamer@email.cz>
2014-10-07 14:05:00 +0200

    Add ElasticSearch support via SearchKick

--- app/controllers/api/crops_controller.rb
+++ app/controllers/api/crops_controller.rb
@@ -3,8 +3,8 @@ module Api
     skip_before_action :authenticate_from_token!, only: [:index, :show]
     def index
       if params[:query].present? && (params[:query].length > 2)
-        q = params[:query].singularize
-        render json: Crop.full_text_search(q).limit(5)
+        q = params[:query]
+        render json: { "crops" => Crop.search(q, fields: ["name^20", "common_names^10", "binomial_name^10", "description"], limit: 5) }
       else
         render json: { "crops" => [] }
       end

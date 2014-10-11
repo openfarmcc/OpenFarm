08cb86753f23d5e8feb6504db4b0a4be268fca7d
Rick Carlino <rick.carlino@gmail.com>
2014-10-11 10:27:46 -0500

    Typo

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -48,7 +48,7 @@ RSpec.configure do |config|
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
-  c.fail_fast = true
+  config.fail_fast = true
   config.order = "random"
   if ENV['DOCS'] == 'true'
     DocYoSelf.config do |c|

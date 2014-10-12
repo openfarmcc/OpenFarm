c93dc6b17e39e71a6d459a23caa22578279b7ad7
Rick Carlino <rick.carlino@gmail.com>
2014-10-12 10:23:26 -0500

    Go fish.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,8 +1,12 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
-  config.secret_key = ENV['SECRET_KEY_BASE']
-
+  if Rails.env.development? or Rails.env.test?
+    token = ('x' * 30) # meets minimum requirement of 30 chars long
+  else
+    token = ENV['SECRET_KEY_BASE']
+  end
+  config.secret_key = token
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class

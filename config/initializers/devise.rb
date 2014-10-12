d35aea60d41334f8e2a35a99fab1de790a84c7d0
Rick Carlino <rick.carlino@gmail.com>
2014-10-12 11:19:11 -0500

    More tweaks

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,12 +1,6 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
-  # if Rails.env.development? or Rails.env.test?
-  #   # I hate this so much. Why can't devise just use ENV[] like a good boy?
-  #   token = ('x' * 30) # meets minimum requirement of 30 chars long
-  # else
-  #   token = ENV['SECRET_KEY_BASE']
-  # end
   config.secret_key = ENV['SECRET_KEY_BASE']
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,

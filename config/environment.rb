be61052a58576b56366b317ead65924a82dc293e
Rick Carlino <rick.carlino@gmail.com>
2014-10-13 07:07:34 -0500

    Cleanup

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ else
   warning = 'You still need to set your ENV variables inside of'\
             ' config/app_environment_variables.rb Take a look at '\
             'config/app_environment_variables.rb.example. Raise an issue on'\
-            'GitHub if you still cant get it working.'
+            ' OpenFarms GitHub page if you still cant get it working.'
   warn warning
 end
 

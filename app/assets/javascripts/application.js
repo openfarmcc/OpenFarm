a898f82c14d5b811835802d94ccab18d9c1d2178
Simon <svansintjan@gmail.com>
2014-10-13 14:15:36 +0700

    Start adding gardens, tested a jslinter, so things were re-spaced

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,42 +20,42 @@
 
 
 //TODO: Need a better folder structure
-$(function(){ 
-	$(document).foundation({
-		  accordion: {
-		    // specify the class used for active (or open) accordion panels
-		    active_class: 'active',
-		    // allow multiple accordion panels to be active at the same time
-		    multi_expand: true,
-		    // allow accordion panels to be closed by clicking on their headers
-		    // setting to false only closes accordion panels when another is opened
-		    toggleable: true
-		  }
-		});
+$(function() {
+  $(document).foundation({
+    accordion: {
+      // specify the class used for active (or open) accordion panels
+      active_class: 'active',
+      // allow multiple accordion panels to be active at the same time
+      multi_expand: true,
+      // allow accordion panels to be closed by clicking on their headers
+      // setting to false only closes accordion panels when another is opened
+      toggleable: true
+    }
+  });
 });
 
 (function(i, s, o, g, r, a, m) {
-    i['GoogleAnalyticsObject'] = r;
-    i[r] = i[r] || function() {
-        (i[r].q = i[r].q || []).push(arguments)
-    }, i[r].l = 1 * new Date();
-    a = s.createElement(o),
-        m = s.getElementsByTagName(o)[0];
-    a.async = 1;
-    a.src = g;
-    m.parentNode.insertBefore(a, m)
+  i['GoogleAnalyticsObject'] = r;
+  i[r] = i[r] || function() {
+    (i[r].q = i[r].q || []).push(arguments);
+  }, i[r].l = 1 * new Date();
+  a = s.createElement(o),
+  m = s.getElementsByTagName(o)[0];
+  a.async = 1;
+  a.src = g;
+  m.parentNode.insertBefore(a, m)
 })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 ga('create', 'UA-54082196-1', 'auto');
 ga('send', 'pageview');
 
 // TODO: move these somewhere actually useful.
 
-var getUrlVar = function(key){
+var getUrlVar = function(key) {
   var result = new RegExp(key + "=([^&]*)", "i").exec(window.location.search);
   return result && unescape(result[1]) || "";
 };
 
-var getIDFromURL = function(key){
+var getIDFromURL = function(key) {
   var result = new RegExp(key + "/([0-9a-f]*)", "i").exec(window.location.pathname);
   return result && unescape(result[1]) || "";
 }

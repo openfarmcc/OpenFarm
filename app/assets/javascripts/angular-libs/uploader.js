cc698c0644538a2a4d3e9a6cdcdc8e7e6c8e5ce6
Rick Carlino <rick.carlino@gmail.com>
2014-10-26 10:20:34 -0500

    More changes.

--- app/assets/javascripts/angular-libs/uploader.js
+++ app/assets/javascripts/angular-libs/uploader.js
@@ -1,14 +1,39 @@
-openFarmModule.directive('uploader', ['$http',
-  function($http) {
-    return {
-      restrict: 'A',
-      link: function($scope, el, attr) {
-        debugger;
-      }
-    };
-}]);
+// Click button to select file
+// Get s3 key ===========================
+    // var handleKey = function(resp, stat){
+    //     options.s3 = resp
+    //   };
+    // var fail = function(resp, stat){
+    //   alert(resp.error);
+    // };
+    // $http
+    //   .get("/api/aws/s3_access_token")
+    //   .success(handleKey)
+    //   .error(fail);
+// upload to s3
+// attach temp URL to specified scope var
+(function(openFarmModule, openFarmApp){
+  var options = {};
+
+  var directiveObject = {
+    restrict: 'A',
+    compile: function($scope, el, attr) {
+      debugger;
+      // el.addEventListener("change", handleFiles, false);
+      // $scope[attr.uploader] = url; // Set the URL into the scope.
+    }
+  }
+
+  var directive = function ($http) {
+    return directiveObject;
+  }
+
+  openFarmModule.directive('uploader', ['$http', directive]);
 
 
-openFarmApp.controller('testbed', function($scope){
-    $scope.hey = '[Yahoo. So 90s](http://www.yahoo.com)'
+  openFarmApp.controller('testbed', function($scope){
+    // debugger;
   });
+
+
+})(openFarmModule, openFarmApp)

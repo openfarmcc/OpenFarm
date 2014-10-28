b15180d3cf14f891b9ddd33802a13d2e99ae5a0e
Simon <svansintjan@gmail.com>
2014-10-28 15:23:33 +0700

    Cleanup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,6 @@ class UsersController < ApplicationController
     @outcome = Users::UpdateUser.run(
       params,
       user: current_user)
-
     if @outcome.errors
       flash[:alert] = @outcome.errors.message_list
       redirect_to(controller: 'users', 

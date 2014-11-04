a8a7ddd1cab93a8800f2f7b86491d4a377bb274c
Rick Carlino <rick.carlino@gmail.com>
2014-11-04 06:41:57 -0600

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,14 @@
 require 'factory_girl_rails'
 if Rails.env != 'production' # <= Prevent catastrophe
   Mongoid.purge!
-  FactoryGirl.create(:user, admin: true,
-                            email: 'admin@admin.com',
-                            password: 'admin123',
-                            password_confirmation: 'admin123')
-  FactoryGirl.create_list(:crop, 40)
+  admin = FactoryGirl.create(:user, admin: true,
+                             email: 'admin@admin.com',
+                             password: 'admin123',
+                             password_confirmation: 'admin123')
+  FactoryGirl.create_list(:stage, 10)
+  FactoryGirl.create_list(:stage_option, 10)
+  FactoryGirl.create_list(:requirement_option, 10)
+  FactoryGirl.create_list(:requirement, 10)
+  FactoryGirl.create_list(:requirement_option, 10)
+  Guide.all.each{ |gde| gde.update_attributes(user: admin) }
 end

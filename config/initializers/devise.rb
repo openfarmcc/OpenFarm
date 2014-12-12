45ce62e5e0df54b6a6729880b394fd189b0ec4df
Rick Carlino <rick.carlino@gmail.com>
2014-12-11 21:16:58 -0600

    Last one, promise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,7 +1,7 @@
 Devise.setup do |config|
   # Devise is really peculiar about how it is given its SECRET_KEY_BASE
   # TODO: Fix this mess.
-  if ["production", "staging"].include?(Rails.env)
+  if ['production', 'staging'].include?(Rails.env)
     devise_acts_weird = ENV['SECRET_KEY_BASE']
   else
     devise_acts_weird = ENV['SECRET_KEY_BASE']  || `rake secret`

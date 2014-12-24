d55aa6eeb50832f262fd453b256c760066ed6d64
Simon <svansintjan@gmail.com>
2014-12-24 14:30:49 +0700

    Update factories to reflect name change

--- test/factories/stage_action_options.rb
+++ test/factories/stage_action_options.rb
@@ -1,5 +1,5 @@
 FactoryGirl.define do
-  factory :action_option do
+  factory :stage_action_option do
     name             { "#{Faker::Name.last_name}" }
     description      { Faker::Lorem.sentence }
   end

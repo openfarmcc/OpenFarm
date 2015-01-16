2f07a257d83188e71ea36e990906ec37d5459b01
Simon <svansintjan@gmail.com>
2015-01-16 17:42:00 +0700

    Initial compatibility score calculation, removed from JS

--- app/models/guide.rb
+++ app/models/guide.rb
@@ -37,27 +37,105 @@ class Guide
     as_json only: [:name, :overview, :crop_id]
   end
 
+  def basic_needs
+    user_environment = user.gardens.first.type
+    user_light = user.gardens.first.average_sun
+    user_ph = user.gardens.first.ph
+    user_soil = user.gardens.first.soil_type
+
+    # TODO: We are duplicating code in the JS
+    # controller here. Clean that one up.
+
+    overlap_l = []
+    overlap_e = []
+    overlap_s = []
+    total_l = []
+    total_e = []
+    total_s = []
+
+    puts stages.to_json
+    stages.each do |s|
+      if s.light
+        overlap_l = overlap_l + s.light.select { |l| l == user_light }
+        total_l = total_l + s.light
+      end
+
+      if s.environment
+        new_array = s.environment.select { |e| e == user_environment }
+        overlap_e = overlap_e + new_array
+        total_e = total_e + s.environment
+      end
+
+      if s.soil
+        puts "old #{overlap_s}"
+        overlap_s = overlap_s + s.soil.select { |s| s == user_soil }
+        puts "new #{overlap_s}"
+        total_s = total_s + s.soil
+      end
+    end
+
+    # We should probably store these in the backend
+
+    basic_needs = [
+      {
+        name: 'Sun / Shade',
+        overlap: overlap_l,
+        have: user_light,
+        need: total_l,
+        percent: !total_l.empty? ? overlap_l.size.to_f / total_l.size : 0
+      },{
+        name: 'pH Range',
+        have: user_ph
+      },{
+        name: 'Temperature'
+      },{
+        name: 'Soil Type',
+        need: total_s,
+        have: user_soil,
+        overlap: overlap_s,
+        percent: !total_s.empty? ? overlap_s.size.to_f / total_s.size : 0
+      },{
+        name: 'Water Use'
+      },{
+        name: 'Location',
+        overlap: overlap_e,
+        need: total_e,
+        have: user_environment,
+        percent: !total_e.empty? ? overlap_e.size.to_f / total_e.size : 0
+      },{
+        name: 'Practices',
+        value: practices
+      },{
+        name: 'Time Commitment'
+      },{
+        name: 'Physical Ability'
+      },{
+        name: 'Time of Year'
+      }
+    ]
+  end
+
   def compatibility_score
-    # Make our random scores consistent based on the first character of the crop name
-    # srand(name[0].ord)
-    # rand(100);
-    nil
+    count = 0
+    sum = basic_needs.inject(0) do |memo, n|
+      count += 1
+      n[:percent] ? memo + n[:percent] : memo
+    end
+    sum.to_f / count
   end
 
   def compatibility_label
-    ''
-    # TODO:
-    # score = compatibility_score
+    score = compatibility_score
 
-    # if score.nil?
-    #   return ''
-    # elsif score > 75
-    #   return 'high'
-    # elsif score > 50
-    #   return 'medium'
-    # else
-    #   return 'low'
-    # end
+    if score.nil?
+      return ''
+    elsif score > 75
+      return 'high'
+    elsif score > 50
+      return 'medium'
+    else
+      return 'low'
+    end
   end
 
   slug :name
--- app/serializers/guide_serializer.rb
+++ app/serializers/guide_serializer.rb
@@ -1,11 +1,19 @@
 class GuideSerializer < ApplicationSerializer
   attributes :_id, :crop_id, :user_id, # :stages,
              :name, :overview, :featured_image, :location,
-             :practices
+             :practices, :compatibility_score, :basic_needs
 
   has_many :stages
 
   def featured_image
     object.featured_image.url
+  end
+
+  def compatibility_score
+    object.compatibility_score
+  end
+
+  def basic_needs
+    object.basic_needs
   end
 end

74e30b6f44a957d227704ed1dd5afeb415279fe8
Rory Aronson <rory@roryaronson.com>
2015-03-04 12:43:01 -0800

    add life-stages sprite, fix up some things

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -85,7 +85,7 @@ $(function() {
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
-      e.preventDefault()
+      preventDefault()
       history.pushState({}, '*', this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {

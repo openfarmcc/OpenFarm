4ca02c5277890e389337d46137715e8d2245b7f0
Rory Aronson <rory@roryaronson.com>
2015-03-04 03:26:53 -0800

    hounding

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -86,7 +86,7 @@ $(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       e.preventDefault()
-      history.pushState({}, "*", this.hash);
+      history.pushState({}, '*', this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html,body').animate({

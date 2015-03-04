05af70184687e3eaa9ecbaf19224983893ebe43b
Rory Aronson <rory@roryaronson.com>
2015-03-04 01:39:35 -0800

    Redo homepage and add 'how it works' popdown

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -85,6 +85,8 @@ $(function() {
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
+      e.preventDefault()
+      history.pushState({}, "*", this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html,body').animate({

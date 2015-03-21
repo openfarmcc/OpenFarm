4df71dd3dc1f8d855895a0fa09a56186051dbdee
Simon <svansintjan@gmail.com>
2015-03-21 12:01:32 +0700

    Hound is fickle

--- app/controllers/api/requirements_controller.rb
+++ app/controllers/api/requirements_controller.rb
@@ -2,29 +2,29 @@ module Api
   class RequirementsController < Api::Controller
     skip_before_action :authenticate_from_token!, only: [:index, :show]
 
-#     def create
-#       @outcome = Requirements::CreateRequirement.run(params,
-#                                                      user: current_user)
-#       respond_with_mutation(:created)
-#     end
+    #     def create
+    #       @outcome = Requirements::CreateRequirement.run(params,
+    #                                                      user: current_user)
+    #       respond_with_mutation(:created)
+    #     end
 
-#     def show
-#       requirement = Requirement.find(params[:id])
-#       render json: requirement
-#     end
+    #     def show
+    #       requirement = Requirement.find(params[:id])
+    #       render json: requirement
+    #     end
 
-#     def update
-#       requirement = Requirement.find(params[:id])
-#       @outcome = Requirements::UpdateRequirement.run(params,
-#                                                      user: current_user,
-#                                                      requirement: requirement)
-#       respond_with_mutation(:ok)
-#     end
+    #     def update
+    #       requirement = Requirement.find(params[:id])
+    #       @outcome = Requirements::UpdateRequirement.run(params,
+    #                                                    user: current_user,
+    #                                               requirement: requirement)
+    #       respond_with_mutation(:ok)
+    #     end
 
-#     def destroy
-#       @outcome = Requirements::DestroyRequirement.run(params,
-#                                                      user: current_user)
-#       respond_with_mutation(:no_content)
-#     end
+    #     def destroy
+    #       @outcome = Requirements::DestroyRequirement.run(params,
+    #                                                      user: current_user)
+    #       respond_with_mutation(:no_content)
+    #     end
   end
 end

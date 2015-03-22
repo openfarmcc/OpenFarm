4f3123c6aa84cb84c26358edc914986a312e5474
Simon <svansintjan@gmail.com>
2015-03-22 08:48:18 +0700

    Fixed commenting on requirementOption

--- app/controllers/api/requirement_options_controller.rb
+++ app/controllers/api/requirement_options_controller.rb
@@ -1,6 +1,6 @@
 module Api
   class RequirementOptionsController < Api::Controller
-    skip_before_action :authenticate_from_token!, only: [:index]
+    # skip_before_action :authenticate_from_token!, only: [:index]
 
     # def index
     #   render json: RequirementOption.all

c2e3589f08b8539727aa135be9e92afc8b9570e5
Simon <svansintjan@gmail.com>
2015-06-08 13:12:05 +0200

    Close #608. Close #618. Make crop selection friendlier and more robust.
    Responses to hound

--- app/controllers/api/guides_controller.rb
+++ app/controllers/api/guides_controller.rb
@@ -3,11 +3,18 @@ module Api
     skip_before_action :authenticate_from_token!, only: [:index, :show]
 
     def create
-      # TODO: something went wrong here, this can be cleaned up.
-      @outcome = Guides::CreateGuide.run(crop_id: params[:crop_id],
+      # TODO: This should be moved into the mutation.
+      # Basically we check if the crop exists, if it doesn't
+      # we create a new one.
+      if params[:crop_id].present?
+        crop_id = params[:crop_id]
+      else
+        crop_id = check_if_crop_exists(params)
+      end
+
+      @outcome = Guides::CreateGuide.run(crop_id: "#{crop_id}",
                                          name: params[:name],
                                          attributes: params,
-                                         # time_span: params[:time_span],
                                          user: current_user)
       respond_with_mutation(:created)
       unless @outcome.errors
@@ -32,6 +39,20 @@ module Api
       @outcome = Guides::DestroyGuide.run(params,
                                           user: current_user)
       respond_with_mutation(:no_content)
+    end
+
+    private
+
+    def check_if_crop_exists(params)
+      crops = Crop.search params[:crop_name], fields: [{ name: :exact }]
+      if crops.count == 0
+        crop = Crop.new( name: params[:crop_name],
+                         common_names: [params[:crop_name],] )
+        crop.save
+        crop.id
+      else
+        crops[0].id
+      end
     end
   end
 end

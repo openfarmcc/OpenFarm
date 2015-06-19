ed890112aa47cfa1bb7bafdcbe7b4f0ea3ed2db4
Cynaria <Elizabeth.J.Cruz@gmail.com>
2015-06-19 13:38:09 -0500

    Add crop_name to serializer

--- app/serializers/guide_serializer.rb
+++ app/serializers/guide_serializer.rb
@@ -1,5 +1,5 @@
 class GuideSerializer < ApplicationSerializer
-  attributes :_id, :crop_id, :user_id, :name, :overview, :featured_image,
+  attributes :_id, :crop_id, :crop_name, :user_id, :name, :overview, :featured_image,
              :location, :practices, :compatibility_score, :basic_needs,
              :completeness_score, :popularity_score
 
@@ -16,5 +16,9 @@ class GuideSerializer < ApplicationSerializer
 
   def basic_needs
     object.basic_needs(current_user)
+  end
+
+  def crop_name
+    object.crop.name
   end
 end

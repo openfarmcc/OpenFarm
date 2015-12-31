abe67a53e7532fe599e5cdb948e063f0cdba36c0
Simon <svansintjan@gmail.com>
2015-12-30 20:35:48 -0800

    Update bootstrap.sh file

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,11 +2,15 @@
 #!/usr/bin/env bash
 
 aptitude    update
-aptitude -y upgrade
-aptitude install -y build-essential
-aptitude install -y cvs git-core
+aptitude -y -qq upgrade
+aptitude install -y -qq build-essential
+aptitude install -y -qq cvs git-core
 
-sudo apt-get install -y git
+sudo apt-get -y -qq install git
+
+sudo apt-get -y -qq install libcurl3 libcurl3-gnutls libcurl4-openssl-dev
+
+echo "OKAY - GOING TO INSTALL OUR OWN THINGS NOW"
 
 echo "--- INSTALLING RVM ---"
 
@@ -27,7 +31,7 @@ rvm --default use 2.2.0
 
 echo "--- INSTALLING ELASTICSEARCH ---"
 
-sudo apt-get install openjdk-7-jre-headless -y
+sudo apt-get -y -qq install openjdk-7-jre-headless -y
 
 wget –quiet https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/deb/elasticsearch/2.0.0/elasticsearch-2.0.0.deb
 sudo dpkg -i elasticsearch-2.0.0.deb
@@ -35,7 +39,7 @@ sudo service elasticsearch start
 
 echo "--- INSTALLING MONGODB ---"
 
-sudo apt-get -y install mongodb
+sudo apt-get -y -qq install mongodb
 
 sudo mkdir /data/ && sudo mkdir /data/db/
 sudo chown -R vagrant /data/db
@@ -43,13 +47,12 @@ sudo service mongodb start
 
 echo "--- OPEN FARM - getting there! ---"
 
-sudo apt-get -y install libcurl3 libcurl3-gnutls libcurl4-openssl-dev
-
 cd /vagrant
 
 gem install bundler
 gem install activesupport -v '4.0.2'
 
+bundle update
 bundle install
 rake db:setup
 echo "ENV['SECRET_KEY_BASE'] = '$(rake secret)'" >> config/app_environment_variables.rb

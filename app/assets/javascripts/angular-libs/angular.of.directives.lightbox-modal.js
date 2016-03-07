c99f10c186c1668d45ddfc9597ebdad39ffb9197
Simon <svansintjan@gmail.com>
2016-03-06 17:13:44 -0800

    Add lightbox CSS

--- app/assets/javascripts/angular-libs/angular.of.directives.lightbox-modal.js
+++ app/assets/javascripts/angular-libs/angular.of.directives.lightbox-modal.js
@@ -47,7 +47,7 @@ openFarmApp.directive('ofLightboxModal', ['$http', '$modal', 'stageService',
           }
         }
       ],
-      template: "<img ng-src={{picture.medium_url}}/ ng-click='open()'>"
+      template: "<img class='lightbox' ng-src={{picture.medium_url}}/ ng-click='open()'>"
     }
   }
 ])

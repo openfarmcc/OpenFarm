83d789e3a9d9187ab1ee08b54d13c4bb88a733cc
TanSA05 <taneea14166@iiitd.ac.in>
2016-07-08 16:17:25 +0530

    Removed double quotes from confirmations_controller_spec and added Ruby 1.9 hash syntax as suggested by Hound

--- spec/features/confirmations_controller_spec.rb
+++ spec/features/confirmations_controller_spec.rb
@@ -4,38 +4,37 @@ class User
   attr_reader :raw_confirmation_token
 end
 
-RSpec.describe ConfirmationsController, :type => :controller do
-
+RSpec.describe ConfirmationsController, type: :controller do
   def visit_user_confirmation_with_token(confirmation_token)
     visit user_confirmation_path(confirmation_token: confirmation_token)
   end
 
-  context "when confirmation token is valid" do
-    context "when user filled the required settings" do
+  context 'when confirmation token is valid' do
+    context 'when user filled the required settings' do
       let(:user) { FactoryGirl.create(:user, :with_user_setting) }
-      it "redirects to member profile page" do
+      it 'redirects to member profile page' do
         user.send_confirmation_instructions
         visit_user_confirmation_with_token(user.raw_confirmation_token)
         expect(response.status).to eq(200)
-        expect(response).to render_template("users/show")
+        expect(response).to render_template('users/show')
       end
     end
-    context "when user has not filled the required settings" do
+    context 'when user has not filled the required settings' do
       let(:user) { FactoryGirl.create(:user) }
-      it "redirects to user details page" do
+      it 'redirects to user details page' do
         user.send_confirmation_instructions
         visit_user_confirmation_with_token(user.raw_confirmation_token)
         expect(response.status).to eq(200)
-        expect(response).to render_template("users/finish")
+        expect(response).to render_template('users/finish')
       end
     end
   end
 
-  context "when confirmation token is invalid" do
-    it "renders error page" do
+  context 'when confirmation token is invalid' do
+    it 'renders error page' do
       visit_user_confirmation_with_token("random_string")
       expect(response.status).to eq(200)
-      expect(response).to render_template("devise/confirmations/new")
+      expect(response).to render_template('devise/confirmations/new')
     end
   end
 end

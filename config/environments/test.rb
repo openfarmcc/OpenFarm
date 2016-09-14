0cab90aaa2d7ca5bd57b542449d84e594d25cdf1
Daniel O'Connor <daniel.oconnor@gmail.com>
2016-09-14 21:07:59 +0930

    Cleanup

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -14,7 +14,7 @@ OpenFarm::Application.configure do
   config.eager_load = false
 
   # Configure static asset server for tests with Cache-Control for performance.
-  config.serve_static_files  = true
+  config.serve_static_files = true
   config.static_cache_control = "public, max-age=3600"
 
   # Show full error reports and disable caching.

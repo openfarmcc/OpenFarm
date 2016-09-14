ef2584a3388036206c66238e876e7ced0ad5e4da
Daniel O'Connor <daniel.oconnor@gmail.com>
2016-09-14 17:35:10 +0930

    Devise 4.1: Now the reconfirmable default is true

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -15,7 +15,6 @@ Devise.setup do |config|
   config.strip_whitespace_keys = [ :email ]
   config.skip_session_storage  = [:http_auth]
   config.stretches = Rails.env.test? ? 1 : 10
-  config.reconfirmable = true
   config.password_length = 8..128
   config.reset_password_within = 6.hours
   config.sign_out_via = :delete

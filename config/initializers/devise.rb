f66b268d18e769623aac8b63464356960e5da162
Daniel O'Connor <daniel.oconnor@gmail.com>
2016-09-14 17:36:18 +0930

    Now the strip_whitespace_keys default is [:email]

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,6 @@ Devise.setup do |config|
   config.mailer_sender = 'team@openfarm.cc'
   require 'devise/orm/mongoid'
   config.case_insensitive_keys = [ :email ]
-  config.strip_whitespace_keys = [ :email ]
   config.stretches = Rails.env.test? ? 1 : 10
   config.password_length = 8..128
   config.reset_password_within = 6.hours

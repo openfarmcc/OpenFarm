d03d249479637debb00bffa40c61f5542240b035
Daniel O'Connor <daniel.oconnor@gmail.com>
2016-09-14 17:35:53 +0930

    Now the sign_out_via default is :delete

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -16,5 +16,4 @@ Devise.setup do |config|
   config.stretches = Rails.env.test? ? 1 : 10
   config.password_length = 8..128
   config.reset_password_within = 6.hours
-  config.sign_out_via = :delete
 end

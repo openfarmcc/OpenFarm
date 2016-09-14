cc69546e4f96a3fbdc405ec60d14687835f9090a
Daniel O'Connor <daniel.oconnor@gmail.com>
2016-09-14 17:35:34 +0930

    Now the skip_session_storage default is [:http_auth]

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -13,7 +13,6 @@ Devise.setup do |config|
   require 'devise/orm/mongoid'
   config.case_insensitive_keys = [ :email ]
   config.strip_whitespace_keys = [ :email ]
-  config.skip_session_storage  = [:http_auth]
   config.stretches = Rails.env.test? ? 1 : 10
   config.password_length = 8..128
   config.reset_password_within = 6.hours

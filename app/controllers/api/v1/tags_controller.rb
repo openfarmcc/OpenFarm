40e4b09b8fcf9b970e1bb450c5f20891b93a60e4
TanSA05 <taneea14166@iiitd.ac.in>
2016-09-30 01:49:52 +0530

    Deleted spec/helper (not req) and fixed tag_controller hound warning

--- app/controllers/api/v1/tags_controller.rb
+++ app/controllers/api/v1/tags_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::TagsController < Api::V1::BaseController
   skip_before_action :authenticate_from_token!, only: [:index]
   def index
-    render json: {tags: CropsTagIndex.where(id: /#{Regexp.escape(params[:query].to_s)}/i).pluck(:id)}
+    render json: { 
+      tags: CropsTagIndex.where(id: /#{Regexp.escape(params[:query].to_s)}/i).pluck(:id)
+    }
   end
 end

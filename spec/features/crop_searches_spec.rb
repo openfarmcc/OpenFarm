bd41ba644ad8cd3d35b38de3a31e175de6f5493b
makena <roselynemakena@gmail.com>
2017-09-19 07:37:37 +0000

    Pending tests - wip

--- spec/features/crop_searches_spec.rb
+++ spec/features/crop_searches_spec.rb
@@ -10,22 +10,7 @@ describe 'Crop search', type: :controller do
   end
   
   it 'handles empty searches', js: true do
-    # visit root_path
-    # fill_in 'q', with: ''
-    # FactoryGirl.create_list(:crop, 10)
-    # Crop.searchkick_index.refresh
-    # click_button 'Search'
-    # title = Crop.first.name
-    # # Look for crop title
-    # expect(page).to have_content(title)
-    # # Look for crop description
-
-    # description = Crop.first.description
-    # expect(page).to have_content(title)
-    # # Image fix to come after configuration is set up
-
-    # image = Crop.first.main_image_path 
-
+    # Pending tests
   end
 
 end

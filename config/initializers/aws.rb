12cd1c1dce22fc2c1315faaeb00588fcb456ea11
Rick Carlino <rick.carlino@gmail.com>
2018-03-23 08:48:26 -0500

    Hound fixes

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,2 +1,4 @@
+# frozen_string_literal: true
+
 # Why? https://github.com/thoughtbot/paperclip/issues/2484
-Aws::VERSION =  Gem.loaded_specs["aws-sdk"].version
+Aws::VERSION = Gem.loaded_specs["aws-sdk"].version

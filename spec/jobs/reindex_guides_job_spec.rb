c7f8eeed386a2ec56b03bf0558cbb81aa5d00c9f
Rick Carlino <rick.carlino@gmail.com>
2018-05-16 08:55:04 -0500

    fix reindex_guides_job_spec.rb

--- spec/jobs/reindex_guides_job_spec.rb
+++ spec/jobs/reindex_guides_job_spec.rb
@@ -2,10 +2,9 @@ require 'spec_helper'
 
 describe ReindexGuidesJob do
   it 'reindexes guides' do
-    FactoryGirl.create(:guide)
-
+    Guide.destroy_all
+    guide = FactoryGirl.create(:guide)
     expect_any_instance_of(Guide).to receive(:reindex_async)
-
     ReindexGuidesJob.new.perform
   end
 end

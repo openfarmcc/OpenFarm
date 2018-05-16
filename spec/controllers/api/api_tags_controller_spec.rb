15d07e92593e40bb62a9146b8e40b26bbf94e638
Rick Carlino <rick.carlino@gmail.com>
2018-05-16 09:02:18 -0500

    api_tags_controller_spec fixes

--- spec/controllers/api/api_tags_controller_spec.rb
+++ spec/controllers/api/api_tags_controller_spec.rb
@@ -5,6 +5,7 @@ describe Api::V1::TagsController, type: :controller do
   let(:user) { FactoryGirl.create(:user) }
 
   before do
+    Crop.destroy_all
     FactoryGirl.create(:crop, name: 'mung bean', tags: 'tagOne')
     FactoryGirl.create(:crop, tags: 'tagTwo')
     Crop.searchkick_index.refresh
@@ -18,7 +19,7 @@ describe Api::V1::TagsController, type: :controller do
   end
 
   it 'returns the tag matching the query' do
-    get 'index', format: :json, query: 'on'
+    get 'index', format: :json, query: 'one'
     expect(response.status).to eq(200)
     expect(json.length).to eq(1)
     expect(json[0]).to eq('tagOne')

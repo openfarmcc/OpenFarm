5eaa530fc16b2628b604e7d80433f9691d202dab
Rick Carlino <rick.carlino@gmail.com>
2019-02-27 14:27:00 -0600

    8 failures, 30 pending

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::UsersController < Api::V1::BaseController
   skip_before_action :authenticate_from_token!, only: [:show]
 
   def gardens_index
-    render json: serialize_models(User.find(params[:id]).gardens,
+    render json: serialize_models(User.find(raw_params[:id]).gardens,
                                   include: ['garden_crops', 'pictures'])
   end
 
@@ -12,7 +12,7 @@ class Api::V1::UsersController < Api::V1::BaseController
   end
 
   def show
-    user = User.find(params[:id])
+    user = User.find(raw_params[:id])
     if Pundit.policy(current_user, user).show?
       render json: serialize_model(user, include: ['user_setting',
                                                    'guides',
@@ -24,10 +24,10 @@ class Api::V1::UsersController < Api::V1::BaseController
 
   def update
     @outcome = Users::UpdateUser.run(
-      attributes: params[:data],
+      attributes: raw_params[:data],
       current_user: current_user,
-      pictures: params[:data][:pictures],
-      user_setting: params[:data][:user_setting],
+      pictures: raw_params[:data][:pictures],
+      user_setting: raw_params[:data][:user_setting],
       id: "#{current_user._id}")
 
     respond_with_mutation(:ok, include: ['user_setting',

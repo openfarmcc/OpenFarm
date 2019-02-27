67bec816b69b03956d15823a49c47b5ee0acc905
Rick Carlino <rick.carlino@gmail.com>
2019-02-27 15:16:49 -0600

    6 failures, 30 pending

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,6 +1,6 @@
-require 'spec_helper'
+require "spec_helper"
 
-describe 'User features', type: :feature do
+describe "User features", type: :feature do
   def host_with_port
     host_with_port = current_host
     if Capybara.current_session.server
@@ -13,33 +13,34 @@ describe 'User features', type: :feature do
     "#{host_with_port}#{Rails.application.config.assets.prefix}/#{relative_path}"
   end
 
-  it 'should return the user profile name when sharing user profile' do
+  it "should return the user profile name when sharing user profile" do
     user = FactoryBot.create(:confirmed_user, :with_user_setting)
     login_as user
     visit user_path(:en, user.id)
-    selector = "meta[property='og:title']" \
-               "[content=\"#{user.display_name} Profile\"]"
-    expect(page).to have_css(selector, visible: false)
+    selector1 = "meta[property='og:title']"
+    meta_content = page.find(selector1, visible: false)["content"]
+    expect(meta_content).to include("#{user.display_name} Profile")
   end
 
-  context 'should handle returning appropriate image when sharing user profile' do
-    context 'when favorite_crop is present' do
+  context "should handle returning appropriate image when sharing user profile" do
+    context "when favorite_crop is present" do
       let(:user) { FactoryBot.create(:user, :with_user_setting) }
-      it 'returns favorite_crop image' do
+      it "returns favorite_crop image" do
         favorite_crop = FactoryBot.create(:crop, :radish)
         favorite_crop_path = favorite_crop.main_image_path
         user.user_setting.favorite_crops << favorite_crop
         login_as user
         visit user_path(:en, user.id)
-        expect(page).to have_css "meta[property='og:image'][content='#{host_with_port}#{favorite_crop_path}']",
-                                  visible: false
+        meta = find("meta[property='og:image']", visible: false)
+        expect(meta).to be
+        expect(meta[:content]).to include("#{host_with_port}#{favorite_crop_path}")
       end
     end
 
-    context 'when favorite_crop is not present' do
+    context "when favorite_crop is not present" do
       let(:user) { FactoryBot.create(:user, :with_user_setting) }
-      it 'returns OpenFarm image' do
-        image = 'openfarm-learn-to-grow-anything-with-community-created-guides'
+      it "returns OpenFarm image" do
+        image = "openfarm-learn-to-grow-anything-with-community-created-guides"
         login_as user
         visit user_path(:en, user.id)
         meta = find("meta[property='og:image']", visible: false)

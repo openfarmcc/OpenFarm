14f208fad91e8fb1f4bd5b1662626e4610fbd610
Rick Carlino <rick.carlino@gmail.com>
2019-02-27 09:49:20 -0600

    13 failures, 30 pending

--- spec/features/confirmations_controller_spec.rb
+++ spec/features/confirmations_controller_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require "spec_helper"
 
 class User
   attr_reader :raw_confirmation_token
@@ -9,32 +9,32 @@ RSpec.describe ConfirmationsController, type: :controller do
     visit user_confirmation_path(confirmation_token: confirmation_token)
   end
 
-  context 'when confirmation token is valid' do
-    context 'when user filled the required settings' do
+  context "when confirmation token is valid" do
+    context "when user filled the required settings" do
       let(:user) { FactoryBot.create(:user, :with_user_setting) }
-      it 'redirects to member profile page' do
+      it "redirects to member profile page" do
         user.send_confirmation_instructions
         visit_user_confirmation_with_token(user.raw_confirmation_token)
         expect(response.status).to eq(200)
-        expect(response).to render_template('users/show')
+        expect(response).to render_template("users/show")
       end
     end
-    context 'when user has not filled the required settings' do
+    context "when user has not filled the required settings" do
       let(:user) { FactoryBot.create(:user) }
-      it 'redirects to user details page' do
+      it "redirects to user details page" do
         user.send_confirmation_instructions
         visit_user_confirmation_with_token(user.raw_confirmation_token)
         expect(response.status).to eq(200)
-        expect(response).to render_template('users/finish')
+        expect(response).to render_template("users/finish")
       end
     end
   end
 
-  context 'when confirmation token is invalid' do
-    it 'renders error page' do
+  context "when confirmation token is invalid" do
+    it "renders error page" do
       visit_user_confirmation_with_token("random_string")
       expect(response.status).to eq(200)
-      expect(response).to render_template('devise/confirmations/new')
+      expect(response).to render_template("devise/confirmations/new")
     end
   end
 end

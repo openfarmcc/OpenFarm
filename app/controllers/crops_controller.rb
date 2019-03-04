6a52f05a4be9303fcac457c9967a2ca004ddaa2d
Rick Carlino <rick.carlino@gmail.com>
2019-03-04 14:36:52 -0600

    [UNSTABLE; SQUASH THIS] Test against prod

--- app/controllers/crops_controller.rb
+++ app/controllers/crops_controller.rb
@@ -18,7 +18,7 @@ class CropsController < ApplicationController
     # darned large. This will start being a problem at around 10k impressions.
     # ~ Simon 07/2016
     impressionist(@crop, unique: [:session_hash])
-    unsorted_guides = @crop.guides.order("impressionist_count asc").last(10) || Guide.none
+    unsorted_guides = @crop.guides.order("impressionist_count asc").last(10) || []
     @guides = Guide.sorted_for_user(unsorted_guides, current_user)
   end
 

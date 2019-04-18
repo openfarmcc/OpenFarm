ec4967e8e67854e4478471df992bea776b99e618
Rick Carlino <rick.carlino@gmail.com>
2019-04-17 19:10:13 -0700

    Hotfix for default icon

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,8 @@
 module ApplicationHelper
+  DEFAULT_ICON_PATH = Rails.root.join("app", "assets", "images", "generic-plant.svg")
+  DEFAULT_ICON      = File.read(DEFAULT_ICON_PATH)
+
   def load_generic_plant_icon
-    file_path = Rails.root.join("app", "assets", "images", "generic-plant.svg")
-    h File.read(file_path)
+    raw(DEFAULT_ICON)
   end
 end

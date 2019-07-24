bd7e94269bebc4f577a8c571eb3d758b9186d8b6
Rick Carlino <rick.carlino@gmail.com>
2019-07-24 11:15:05 -0500

    Actually, lets just use Heroku ACM instead of Lets Encrypt gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,36 +4,35 @@ OpenFarm::Application.configure do
   config.action_dispatch.perform_deep_munge = false
   config.cache_classes = true
   config.eager_load = true
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
   config.serve_static_files = false
   config.assets.js_compressor = Uglifier.new(harmony: true)
   config.assets.compile = true
   config.assets.digest = true
-  config.assets.version = '1.0'
+  config.assets.version = "1.0"
   config.force_ssl = true
-  config.middleware.insert_before ActionDispatch::SSL, Letsencrypt::Middleware
   config.log_level = :info
-  config.action_mailer.smtp_settings = {  address:   'smtp.mandrillapp.com',
-                                          port:      587,
-                                          user_name: ENV['MANDRILL_USERNAME'],
-                                          password:  ENV['MANDRILL_APIKEY'] }
+  config.action_mailer.smtp_settings = { address: "smtp.mandrillapp.com",
+                                        port: 587,
+                                        user_name: ENV["MANDRILL_USERNAME"],
+                                        password: ENV["MANDRILL_APIKEY"] }
 
   config.middleware.use ExceptionNotification::Rack,
     email: {
-      email_prefix: '[OpenFarm Errors] ',
+      email_prefix: "[OpenFarm Errors] ",
       sender_address: %{"notifier" <notifier@openfarm.cc>},
-      exception_recipients: ENV['ALERTS'].to_s.split('|')
+      exception_recipients: ENV["ALERTS"].to_s.split("|"),
     },
     ignore_exceptions: [
-      'AbstractController::ActionNotFound',
-      'ActionController::InvalidAuthenticityToken',
-      'ActionController::RoutingError',
-      'ActionController::UnknownFormat',
-      'ActionView::MissingTemplate',
-      'Mongoid::Errors::DocumentNotFound',
+      "AbstractController::ActionNotFound",
+      "ActionController::InvalidAuthenticityToken",
+      "ActionController::RoutingError",
+      "ActionController::UnknownFormat",
+      "ActionView::MissingTemplate",
+      "Mongoid::Errors::DocumentNotFound",
     ]
-  config.action_mailer.default_url_options = { host: 'openfarm.cc' }
+  config.action_mailer.default_url_options = { host: "openfarm.cc" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
@@ -41,13 +40,12 @@ OpenFarm::Application.configure do
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
   options = { storage: :s3,
-              s3_protocol: :https,
-              path: '/:rails_env/media/:class/:attachment/:id.:extension',
-              s3_credentials: { bucket: ENV['S3_BUCKET_NAME'],
-                                s3_protocol: :https,
-                                access_key_id: ENV['SERVER_S3_ACCESS_KEY'],
-                                secret_access_key: ENV['SERVER_S3_SECRET_KEY'] } }
+             s3_protocol: :https,
+             path: "/:rails_env/media/:class/:attachment/:id.:extension",
+             s3_credentials: { bucket: ENV["S3_BUCKET_NAME"],
+                              s3_protocol: :https,
+                              access_key_id: ENV["SERVER_S3_ACCESS_KEY"],
+                              secret_access_key: ENV["SERVER_S3_SECRET_KEY"] } }
 
   Paperclip::Attachment.default_options.merge!(options)
-
 end

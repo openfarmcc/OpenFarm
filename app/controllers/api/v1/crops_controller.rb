38eed752003c72a89b5cba63660149ef201d4181
Brenda <brenda@wallace.net.nz>
2019-08-30 14:48:41 +1200

    Rubocop lint

--- app/controllers/api/v1/crops_controller.rb
+++ app/controllers/api/v1/crops_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class Api::V1::CropsController < Api::V1::BaseController
-  skip_before_action :authenticate_from_token!, only: [:index, :show]
+  skip_before_action :authenticate_from_token!, only: %i[index show]
 
   def index
     render json: should_perform_search? ? search_result : empty_search_result
@@ -7,13 +9,13 @@ class Api::V1::CropsController < Api::V1::BaseController
 
   def show
     crop = Crop.find(raw_params[:id])
-    render json: serialize_model(crop, include: ["pictures", "companions"])
+    render json: serialize_model(crop, include: ['pictures', 'companions'])
   end
 
   def create
     @outcome = Crops::CreateCrop.run(raw_params[:data],
                                      user: current_user)
-    respond_with_mutation(:ok, include: ["pictures"])
+    respond_with_mutation(:ok, include: ['pictures'])
   end
 
   def update
@@ -29,7 +31,7 @@ class Api::V1::CropsController < Api::V1::BaseController
     @outcome = Crops::UpdateCrop.run(raw_params[:data],
                                      user: current_user,
                                      id: raw_params[:id])
-    respond_with_mutation(:ok, include: ["pictures"])
+    respond_with_mutation(:ok, include: ['pictures'])
   end
 
   private
@@ -37,12 +39,12 @@ class Api::V1::CropsController < Api::V1::BaseController
   def crops
     @crops ||= Crop.search(raw_params[:filter],
                            limit: 25,
-                           operator: "or", # partial: true,
+                           operator: 'or', # partial: true,
                            misspellings: { distance: 1 },
-                           fields: ["name^20",
-                                    "common_names^10",
-                                    "binomial_name^10",
-                                    "description"],
+                           fields: ['name^20',
+                                    'common_names^10',
+                                    'binomial_name^10',
+                                    'description'],
                            boost_by: [:guides_count])
   end
 
@@ -57,6 +59,6 @@ class Api::V1::CropsController < Api::V1::BaseController
   end
 
   def search_result
-    serialize_models(crops, include: ["pictures"])
+    serialize_models(crops, include: ['pictures'])
   end
 end

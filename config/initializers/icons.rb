813e51f14557adebca38d1f355a3de5f4c129911
Brenda <brenda@wallace.net.nz>
2019-11-10 20:41:42 +1300

    Seperate paths in cross OS way

--- config/initializers/icons.rb
+++ config/initializers/icons.rb
@@ -1,2 +1,4 @@
-DEFAULT_ICON_PATH = Rails.root.join("app", "assets", "images", "generic-plant.svg")
+# frozen_string_literal: true
+
+DEFAULT_ICON_PATH = Rails.root.join('app', 'assets', 'images', 'generic-plant.svg')
 DEFAULT_ICON      = File.read(DEFAULT_ICON_PATH)

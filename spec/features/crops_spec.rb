5457d9fb589242bd7c473a6ad67074863297dcf2
Brenda Wallace <brenda.wallace@dia.govt.nz>
2019-11-10 17:48:11 +1300

    Tests for editing crops
    diff --git a/spec/features/crops_spec.rb b/spec/features/crops_spec.rb
    index ad5c47ef..800771df 100644
    --- a/spec/features/crops_spec.rb
    +++ b/spec/features/crops_spec.rb
    @@ -10,8 +10,19 @@ describe 'Crop pages', type: :controller do
         expect(page).to have_content('radish')
       end
    
    +  it 'edits crops' do
    +    login_as user
    +    visit "/crops/#{crop._id}"
    +    expect(page).to have_link('Edit crop')
    +    click_link 'Edit crop'
    +    expect(page).to have_content('Edit radish')
    +    fill_in id: 'crop_description', with: 'easter eggs'
    +    click_button 'Save Crop'
    +    expect(page).to have_content 'easter eggs'
    +  end
    +
    -  it 'uploads pictures'
    +  pending 'uploads pictures'
    
       it 'shows create page for a crop' do
         login_as user

--- spec/features/crops_spec.rb
+++ spec/features/crops_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
-describe 'Crop pages', type: :controller do
-  include IntegrationHelper
+describe 'Crop pages', :js do
   let(:user) { FactoryBot.create(:user) }
   let(:crop) { FactoryBot.create(:crop, name: 'radish') }
 
@@ -10,8 +9,23 @@ describe 'Crop pages', type: :controller do
     expect(page).to have_content('radish')
   end
 
+  it 'edits crops' do
+    login_as user
+    visit "/crops/#{crop._id}"
+    expect(page).to have_link('Edit crop')
+    click_link 'Edit crop'
+    expect(page).to have_content('Edit radish')
+
+    fill_in id: 'crop_binomial_name', with: 'Raphanus sativus'
+    fill_in id: 'crop_description', with: 'easter eggs'
+
+    click_button 'Save Crop'
+    expect(page).to have_text 'Raphanus sativus'
+    expect(page).to have_text 'easter eggs'
+  end
+
   # TODO: How do we test this?
-  it 'uploads pictures'
+  pending 'uploads pictures'
 
   it 'shows create page for a crop' do
     login_as user

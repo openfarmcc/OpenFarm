b5ca39b603159b75effb68dab9d1c5085d2b549f
Brenda Wallace <brenda@wallace.net.nz>
2019-11-12 16:19:41 +1300

    Re-instate editing of crop icons (#1109)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,2 @@
 module ApplicationHelper
-  DEFAULT_ICON_PATH = Rails.root.join("app", "assets", "images", "generic-plant.svg")
-  DEFAULT_ICON      = File.read(DEFAULT_ICON_PATH)
-
-  def load_generic_plant_icon
-    raw(DEFAULT_ICON)
-  end
 end
---
+++ config/initializers/icons.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+DEFAULT_ICON_PATH = Rails.root.join('app', 'assets', 'images', 'generic-plant.svg')
+DEFAULT_ICON      = File.read(DEFAULT_ICON_PATH)

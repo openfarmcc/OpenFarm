16f34a62cd28756c4f94850ee29ca77e4a226687
Brenda Wallace <brenda@wallace.net.nz>
2019-12-06 16:22:31 +1300

    Auto corrected by following Lint Ruby Layout/EmptyLines (#1129)

--- app/models/user_setting.rb
+++ app/models/user_setting.rb
@@ -10,7 +10,6 @@ class UserSetting
   field :years_experience, type: Integer
   field :units,            type: String
 
-
   field :processing_pictures, type: Integer, default: 0
   embeds_many :pictures, as: :photographic
   accepts_nested_attributes_for :pictures

4e22cbc91c50aea8ce62ea315743ae893f16701d
Brenda Wallace <brenda@brenda.nz>
2021-01-20 10:43:57 +1300

    Corrected name of file

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ else
   warning = [
     'You might still need to set your ENV variables inside of',
     'config/app_environment_variables.rb Take a look at',
-    'config/app_environment_variables.rb.example. Raise an issue on',
+    'config/app_environment_variables.example.rb. Raise an issue on',
     'OpenFarms GitHub page if you still cant get it working. If you',
     'are seeing this from within your CI or production server, you can',
     'ignore this message. Make sure you set your ENV vars, though.'
